7af58130c18c952af607f428c72b395c5aa0480d
tktkaj <tktkaj@soonsil.ac.kr>
2025-01-21 13:51:21 +0900

    FGMKDT00401 클릭이벤트 수정

--- src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00401.jsx
+++ src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00401.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function FGMKDT00401() {
   const navNames = ["상품설명", "상세정보", "후기(29)", "문의"];
+  const [selectedNav, setSelectedNav] = useState(null);
+
   return (
     <DetailNavBox>
       {navNames.map((navName, index) => (
-        <DetailButton key={navName + index}>{navName}</DetailButton>
+        <DetailButton
+          key={navName + index}
+          onClick={() => setSelectedNav(index)}
+          isSelected={selectedNav === index}
+        >
+          {navName}
+        </DetailButton>
       ))}
     </DetailNavBox>
   );
@@ -22,10 +30,14 @@ const DetailNavBox = styled.div`
 `;
 
 const DetailButton = styled.button`
-  background-color: #fafafa;
+  background-color: ${(props) => (props.isSelected ? "#ffffff" : "#fafafa")};
   border: 1px solid #eee;
   width: 253px;
   padding: 20px 0;
+  font-weight: ${(props) => (props.isSelected ? "bold" : "normal")};
+  color: ${(props) => (props.isSelected ? "#5f0080" : "#666666")};
+  border-bottom: ${(props) =>
+    props.isSelected ? "none" : "1px solid #eee"};
 `;
 
 export default FGMKDT00401;
