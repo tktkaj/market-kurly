bff9672c5c3ae56224e287b9cef3eb29d9d3f79c
tktkaj <tktkaj@soonsil.ac.kr>
2025-01-21 10:19:40 +0900

    FGMKDT004 style 적용

--- src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00402.jsx
+++ src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00402.jsx
@@ -3,17 +3,26 @@ import styled from "styled-components";
 function FGMKDT00402({ price }) {
   return (
     <SaleNowCouponLayout>
-      <StyledSpan>첫구매하면
-        <StyledImpactSpan> { price }원 </StyledImpactSpan>
+      <StyledSpan>
+        첫구매하면
+        <StyledImpactSpan> {price}원 </StyledImpactSpan>
         즉시 할인
       </StyledSpan>
     </SaleNowCouponLayout>
   );
 }
-const SaleNowCouponLayout = styled.div``;
-const StyledSpan = styled.span``;
+const SaleNowCouponLayout = styled.div`
+  background-color: #bd76ff14;
+  padding: 10px 14px 10px 16px;
+  width: 350px;
+`;
+const StyledSpan = styled.span`
+  font-size: 0.8rem;
+`;
 const StyledImpactSpan = styled.span`
   color: #5f0080;
+  font-weight: bold;
+  font-size: 0.8rem;
 `;
 
 export default FGMKDT00402;
--- src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00403.jsx
+++ src/components/FG/FGMK/FGMKDT/FGMKDT004/FGMKDT00403.jsx
@@ -72,23 +72,66 @@ function FGMKDT00403({ setTotalPrice }) {
 
   const bellIconSrc =
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIHN0cm9rZT0iI0NDQyIgc3Ryb2tlLXdpZHRoPSIxLjYiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTEyLjY2NiAyM2EzLjMzMyAzLjMzMyAwIDEgMCA2LjY2NiAwIi8+CiAgICAgICAgPHBhdGggZD0iTTI1Ljk5OCAyMi43MzhINmwuMDEzLS4wM2MuMDc2LS4xMzUuNDcxLS43MDQgMS4xODYtMS43MDlsLjc1LTEuMDV2LTYuNjM1YzAtNC40ODUgMy40MzgtOC4xNCA3Ljc0MS04LjMwOEwxNiA1YzQuNDQ2IDAgOC4wNSAzLjcyMiA4LjA1IDguMzE0djYuNjM0bDEuNzA3IDIuNDExYy4xNzMuMjUzLjI1NC4zOC4yNDIuMzh6IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KICAgIDwvZz4KPC9zdmc+Cg==";
+
   return (
     <ShoppingOptionsLayout>
       <ProductTitle>
         <div>
           <DeliveryDiv>{product.dileveryType}</DeliveryDiv>
-          <h3>{product.title}</h3>
-          <h1>{product.subtitle}</h1>
+          <h3 style={{ fontSize: "1.3rem", margin: "0px", marginTop: "10px" }}>
+            {product.title}
+          </h3>
+          <h1
+            style={{
+              margin: "0px",
+              marginTop: "10px",
+              fontSize: "0.8rem",
+              color: "#b5b5b5",
+            }}
+          >
+            {product.subtitle}
+          </h1>
         </div>
         <div style={{ display: "flex", alignItems: "center" }}>{shareIcon}</div>
       </ProductTitle>
       <DisPriceDiv>
-        <span>{product.disPercent}</span>
-        <span>{product.disPrice}</span>
+        <span
+          style={{ fontSize: "1.6rem", color: "#fa622f", fontWeight: "bold" }}
+        >
+          {product.disPercent}
+        </span>
+        <span style={{ fontSize: "1.6rem", fontWeight: "bold" }}>
+          {product.disPrice}
+        </span>
       </DisPriceDiv>
-      <OriPriceDiv>{product.oriPrice}</OriPriceDiv>
+      <OriPriceDiv>
+        {product.oriPrice}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="21"
+          viewBox="0 0 21 21"
+          fill="none"
+        >
+          <circle
+            cx="10.5"
+            cy="10.5"
+            r="6.9"
+            stroke="#ccc"
+            stroke-width="1.2"
+          ></circle>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M9.19735 8.7317H7.80005C7.84762 7.32251 8.81681 6.2998 10.5828 6.2998C12.2119 6.2998 13.3 7.23926 13.3 8.55332C13.3 9.46305 12.8482 10.0993 12.0395 10.5809C11.2606 11.0387 11.0406 11.342 11.0406 11.9306V12.2695H9.66113L9.65518 11.8652C9.60167 10.9733 9.94654 10.4382 10.7671 9.95656L10.9414 9.84757C11.5443 9.45619 11.7541 9.14643 11.7541 8.60683C11.7541 7.98845 11.2546 7.54251 10.5055 7.54251C9.73843 7.54251 9.24491 8.00629 9.19735 8.7317ZM9.42924 14.1603C9.42924 14.7312 9.82167 15.1058 10.4163 15.1058C11.0228 15.1058 11.4033 14.7312 11.4033 14.1603C11.4033 13.5836 11.0228 13.209 10.4163 13.209C9.82167 13.209 9.42924 13.5836 9.42924 14.1603Z"
+            fill="#CCCCCC"
+          ></path>
+        </svg>
+      </OriPriceDiv>
       <OriginDiv>{product.origin}</OriginDiv>
-      <FGMKDT00402 price="10000" />
+      <div style={{ marginTop: "10px" }}>
+        <FGMKDT00402 price="10000" />
+      </div>
       <DetailList>
         {product.productDetail.map((item, index) => (
           <DetailItem key={item + index}>
@@ -104,9 +147,11 @@ function FGMKDT00403({ setTotalPrice }) {
         </DetailItem>
       </DetailList>
       <TotalPriceDiv>
-        총 상품금액:
-        {/* 변수로 바꿀것 */}
-        <span>10,965원</span>
+        <span style={{ fontSize: "0.7rem" }}>
+          총 상품금액:
+          <span style={{ fontSize: "1.8rem", fontWeight: "bold",marginLeft:"10px"  }}>10,965</span>
+          <span style={{ fontSize: "1.3rem", fontWeight: "bold",marginLeft:"5px" }}>원</span>
+        </span>
       </TotalPriceDiv>
       <UtilButtonDiv>
         <IconButtonDiv>
@@ -127,27 +172,57 @@ function FGMKDT00403({ setTotalPrice }) {
   );
 }
 const ShoppingOptionsLayout = styled.div``;
-const DeliveryDiv = styled.div``;
+const DeliveryDiv = styled.div`
+  font-size: 0.8rem;
+  color: #999;
+`;
 const ProductTitle = styled.div`
   display: flex;
   align-items: center;
 `;
-const DisPriceDiv = styled.div``;
-const OriPriceDiv = styled.div``;
-const OriginDiv = styled.div``;
+const DisPriceDiv = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const OriPriceDiv = styled.div`
+  text-decoration: line-through;
+  color: #b5b5b5;
+`;
+const OriginDiv = styled.div`
+  margin-top: 10px;
+  font-size: 1.4rem;
+`;
 const DetailList = styled.ul`
   list-style: none;
+  padding: 0;
+  border-bottom: 1px solid #f4f4f4;
 `;
 const DetailItem = styled.li`
+  border-top: 1px solid #f4f4f4;
+  padding: 15px 0px;
   display: flex;
   gap: 10px;
 `;
-const StyledDt = styled.dt``;
-const StyledDd = styled.dd``;
-const TotalPriceDiv = styled.div``;
+const StyledDt = styled.dt`
+  font-size: 0.8rem;
+  width: 80px;
+  color: #666;
+`;
+const StyledDd = styled.dd`
+  font-size: 0.8rem;
+  width: 300px;
+  color: #666;
+`;
+const TotalPriceDiv = styled.div`
+  display: flex;
+  justify-content: end;
+`;
 const UtilButtonDiv = styled.div`
   display: flex;
   gap: 10px;
+  margin-top: 20px;
 `;
 const IconButtonDiv = styled.div`
   display: flex;
